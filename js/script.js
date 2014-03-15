e8083e4aad2685d953af97c2f37d901ac54c275c
Gabriel M. Muresan <muresan.gabriel.m@gmail.com>
2014-03-16 00:59:02 +0100

    added dragging and dropping options

--- js/script.js
+++ js/script.js
@@ -39,14 +39,71 @@ _onReady(function() {
             fadeIn(el);
         }
     }
-//    var notes = document.querySelectorAll(".note > .content");
-//    for (var i = 0; i < notes.length; ++i) {
-//        notes[i].addEventListener("click", function(event)
-//                                           {
-//                                               this.style.height=0;
-//                                               this.style.overflow = "hidden";
-//                                           }, false);
-//    }
+    var dragSrcEl = null;
+    function handleDragStart(e) {
+        this.style.opacity = '0.4';  // this / e.target is the source node.
+
+        dragSrcEl = this;
+
+        e.dataTransfer.effectAllowed = 'move';
+        e.dataTransfer.setData(Element, this);
+    }
+    function handleDrop(e) {
+        // this / e.target is current target element.
+
+        if (e.stopPropagation) {
+            e.stopPropagation(); // stops the browser from redirecting.
+        }
+
+        // See the section on the DataTransfer object.
+
+        return false;
+    }
+
+    function handleDragEnd(e) {
+        // this/e.target is the source node.
+        fadeIn(this);
+        [].forEach.call(dropables, function(col) {
+            col.classList.remove('over');
+        });
+    }
+    function handleDragOver(e) {
+        if (e.preventDefault) {
+            e.preventDefault(); // Necessary. Allows us to drop.
+        }
+        e.dataTransfer.dropEffect = 'move';  // See the section on the DataTransfer object.
+        return false;
+    }
+    function handleDragEnter(e) {
+        // this / e.target is the current hover target.
+        this.classList.add('over');
+    }
+
+    function handleDragLeave(e) {
+        this.classList.remove('over');  // this / e.target is previous target element.
+    }
+    function handleDrop(e) {
+        // this/e.target is current target element.
+
+        if (e.stopPropagation) {
+            e.stopPropagation(); // Stops some browsers from redirecting.
+        }
+        this.appendChild(dragSrcEl);
+        return false;
+    }
+    var draggables = document.querySelectorAll('[draggable]');
+    [].forEach.call(draggables, function(col) {
+        col.addEventListener('dragstart', handleDragStart, false);
+        col.addEventListener('dragend', handleDragEnd, false);
+    });
+    var dropables = document.querySelectorAll('[dropable]');
+    [].forEach.call(dropables, function(col) {
+        col.addEventListener('dragover', handleDragOver, false);
+        col.addEventListener('dragenter', handleDragEnter, false);
+        col.addEventListener('dragleave', handleDragLeave, false);
+        col.addEventListener('drop', handleDrop, false);
+    });
+
     var moveRightNodes = document.querySelectorAll(".moveRight");
     for (var i = 0; i < moveRightNodes.length; ++i) {
         moveRightNodes[i].addEventListener("click", function(event)
